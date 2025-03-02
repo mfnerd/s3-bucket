287c40178987486ee43086ee2b88249c9200c6e8
Devan Shepard <wdevanshepard@gmail.com>
2025-03-02 14:23:39 -0600

    Create main.tf

---
+++ provider "aws" {   region = "us-east-2" }  terraform {   required_providers {     aws = {       source  = "hashicorp/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
